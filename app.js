e2ead73f8462736b92d922120563f987b91d0658
WELLINGTON NOVAIS <well96oliveira@gmail.com>
2025-02-08 20:03:10 -0300

    Refatorar o código e adicionar novas ções

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-
+/*
+Nome: Sorteador de amigos
+Autor: Wellington Novais
+Data: 07/02/2025
+Descrição: Sorteia um nome aleatoriamente dentre os adicionados à lista
+*/
 let amigos = [];
-let nome = '';
-//Esta função e acionada quando o botão "adicionar" é clicado e faz o teste no campo imput do HTML, caso o campo tenha algum valor, esse valor é adicionado na ultima posição da lista "amigos" caso contrário chama a função "mensagemErro"
+let id = 'listaAmigos';
+//Adiciona o nome digitado pelo usuário à lista "amigos". Caso o campo esteja vazio ou o nome seja repetido chama uma função de erro equivalente
 function adicionandoAmigos() {
     let jaExiste = amigos.includes(document.querySelector('input').value);
     if(document.querySelector('input').value != '' && jaExiste === false) {
         nome = document.querySelector('input').value;
         amigos.push(document.querySelector('input').value) && limpaNome();
-        exibirListaDeAmigos('li', nome);
+        exibirListaDeAmigos();
     } else {
         if(jaExiste == true) {
             limpaNome();
             mensagemNomeRepetido();
         } else {
-            mensagemErro();
+            mensagemCampoVazio();
         }
     }
 }
@@ -23,19 +28,37 @@ function limpaNome() {
     document.querySelector('input').value = '';
 }
 
-//Exibe um alert de erro
-function mensagemErro() {
+//Exibe um alert de erro devido ao campo vazio
+function mensagemCampoVazio() {
     alert('ERRO! Para adicionar um amigo o campo não pode estar vazio.');
 }
 
-//Exibe os nomes dos amigos
-function exibirListaDeAmigos(tag,nome) {
-    
-    let nomeDigitado = document.getElementById('listaAmigos');
-    nomeDigitado.innerHTML = nome; 
+//Exibe um alert de erro devido a nome repetido
+function mensagemNomeRepetido(){
+    alert('ERRO! Esse amigo já foi adicionado, digite um nome diferente.');
 }
 
-//Exibe um alert devido a nome repetido
-function mensagemNomeRepetido(){
-    alert('ERRO! Esse amigo já foi adicionado, escreva um nome diferente.');
+//Exibe uma lista com os nomes dos amigos
+function exibirListaDeAmigos() {
+    let lista = document.getElementById(id);
+    lista.innerHTML = '';
+    for (let contador = 0; contador < amigos.length; contador++) {
+        let novoAmigo = amigos[contador];
+        lista = document.getElementById(id);
+        let novoElemento = document.createElement('li');
+        lista.appendChild(novoElemento);
+        novoElemento.innerHTML = novoAmigo;
+    }
+}
+
+//limpa a lista e sorteia um amigo aleatório destre os presentes na lista
+function sortearAmigo() {
+    if(amigos != '') {
+        let limpaLista = document.getElementById(id);
+        limpaLista.innerHTML = '';
+        let indice = Math.floor(Math.random()*amigos.length);
+        let amigoSorteado = amigos[indice];
+        let textoResultado = document.getElementById('resultado');
+        textoResultado.innerHTML = (`O amigo secreto sorteado é: ${amigoSorteado}`);
+    }
 }
